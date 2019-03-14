43f8da7c0cccf3b86010ca947c125334b15d3f49
alexandra <alexaspalato@gmail.com>
2019-03-14 19:47:48 +0100

    single post

--- packages/gatsby-theme-tabor/src/templates/posts/single.js
+++ packages/gatsby-theme-tabor/src/templates/posts/single.js
@@ -1,55 +1,84 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
-import Layout from "../../components/layout"
+import Layout from "../../components/Layout"
+import PostEntryMeta from "../../components/PostEntryMeta"
 import PostEntryTitle from "../../components/PostEntryTitle"
+import PostEntryMedia from "../../components/PostEntryMedia"
+import SEO from "../../components/SEO"
 
-const SinglePost = props => {
+const renderTermNodes = (nodes, termType) => (
+  <span className="cat-links header-font extra-small medium smooth dark-gray">
+    {nodes.map(term => (
+      <Link to={`/blog/${termType}/${term.slug}`}>{term.name}</Link>
+    ))}
+  </span>
+)
+
+const renderTerms = (categoryNodes = [], tagNodes = []) => (
+  <>
+    {categoryNodes ? renderTermNodes(categoryNodes, "category") : null}
+    {tagNodes && tagNodes.length ? renderTermNodes(tagNodes, "tag") : null}
+  </>
+)
+
+const Post = props => {
   const {
+    location,
     data: {
       wpgraphql: { post },
     },
   } = props
-  const { id, postId, title, content } = post
-
+  const { title, content } = post
   return (
-    <Layout>
-      <article
-        data-id={id}
-        id={`post-${postId}`}
-        className={`post-${postId} post type-post status-publish format-standard hentry category-react tag-accessibility tag-gatsby entry`}
-      >
-        <header className="entry-header">
+    <Layout location={location}>
+      <SEO title={`${post.title}`} />
+      <div className="post-wrapper">
+        <header className="entry-header top-spacer bottom-spacer">
           <PostEntryTitle
-            post={post}
             location="single"
-            titleClass="entry-title"
+            post={post}
+            titleClass="entry-title h1"
           />
-          <div className="entry-meta" />
-          {/* .meta-info */}
+
+          <PostEntryMeta post={post} />
         </header>
+        {post.featuredImage && <PostEntryMedia post={post} location="single" />}
 
         <div
           className="entry-content"
           dangerouslySetInnerHTML={{ __html: content }}
         />
-        {/* .entry-content */}
+      </div>
+      <footer className="entry-footer flex justify-between">
+        <div className="flex justify-start items-center">
+          <a
+            className="comments-trigger button button--mobile-fullwidth center-align"
+            id="comments-trigger"
+            href="#" //TODO: setup the comment functionality
+          >
+            <span className="display-none">Close Comments</span>
 
-        <footer className="entry-footer" />
-      </article>
-      {/* #post-${ID} */}
+            <span className="display-inline-block">Leave a Comment</span>
+          </a>
+        </div>
+        {/* TODO: add taxonomies to a new component */}
+        <div className="entry-footer__taxonomy justify-end self-center items-center">
+          {post.categories.nodes.length || post.tags.nodes.length
+            ? renderTerms(post.categories.nodes, post.tags.nodes)
+            : null}
+        </div>
+      </footer>
     </Layout>
   )
 }
 
-export default SinglePost
+export default Post
 
 export const pageQuery = graphql`
   query GET_POST($id: ID!) {
     wpgraphql {
       post(id: $id) {
         title
-        id
-        postId
         content
         uri
         featuredImage {
